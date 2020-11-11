32c231b3799b909cad167806499caed730e633b3
TrishynaAnna <trishka1998anutka@gmail.com>
2020-11-11 12:03:14 +0200

    add

---
+++ build/js/scripts.js
@@ -0,0 +1,1 @@
+"use strict";var button=document.querySelector(".header-btn__inner"),header=document.querySelector(".header");button.addEventListener("click",function(){button.classList.toggle("is-active"),header.classList.toggle("nav-open")});
