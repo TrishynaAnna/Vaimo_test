e92e58929602065c7eb815fc70d68684961c2c65
TrishynaAnna <trishka1998anutka@gmail.com>
2020-11-11 12:10:31 +0200

    add

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,1 +1,17 @@
-"use strict";var button=document.querySelector(".header-btn__inner"),header=document.querySelector(".header");button.addEventListener("click",function(){button.classList.toggle("is-active"),header.classList.toggle("nav-open")});
+"use strict";
+
+// $(function() {
+//   $(".header-btn").on("click", function(){
+//     $(".menu").addClass("active");
+//   });
+//
+//   $(".close-btn").on("click", function(){
+//     $(".menu").removeClass("active");
+//   });
+// });
+var button = document.querySelector('.header-btn__inner');
+var header = document.querySelector('.header');
+button.addEventListener('click', function () {
+  button.classList.toggle('is-active');
+  header.classList.toggle('nav-open');
+});
